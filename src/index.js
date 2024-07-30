5bb397d2ca171598920b870c112f81e088ac83d9
Boris Drokin <drokcin.b@gmail.com>
2024-07-30 19:03:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = "";
+
+    n = Math.abs(n) + '';  
+
+    for (let i= n.length - 1; i >= 0; i--) {
+        result = result + n.slice(i, i + 1 );
+    }
+
+    return +result;
 }
